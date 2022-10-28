b1bc600f9045fa2ea604dc81135814e1306d5719
JokubasD <jokubasdimsa@gmail.com>
2022-10-28 08:56:40 +0300

    first modal done

---
+++ assets/scripts/app.js
@@ -0,0 +1,29 @@
+'use strict';
+console.log('app.js');
+
+//Targeting ==========================================================
+
+const els = {
+    btn: {
+        addMovie: document.getElementById('add-movie-btn'),
+    },
+    modals: {
+        movieModal: document.getElementById('add-modal'),
+    },
+    backdrop: document.getElementById('backdrop'),
+}
+console.log(els)
+
+
+//Event Listeners ==========================================================
+//==========================================================================
+//==========================================================================
+
+els.btn.addMovie.addEventListener('click', () => {
+    els.modals.movieModal.classList.add('visible')
+    els.backdrop.classList.add('visible')
+})
+
+els.backdrop.addEventListener('click', () => {
+    els.backdrop.classList.remove('visible')
+})
